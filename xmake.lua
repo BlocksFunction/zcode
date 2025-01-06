fff463aa8720d21b6f1ab26b0d07f266038b43db
BlocksFunction <3398817447@qq.com>
2025-01-06 20:28:46 +0800

    e

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,8 @@
 add_rules("mode.debug", "mode.release")
-set_languages("c++26")
 
 target("zcode")
     set_kind("binary")
-    add_files("src/core/*.cpp")
+    add_files("src/*.cpp")
 
 --
 -- If you want to known more usage about xmake, please see https://xmake.io
