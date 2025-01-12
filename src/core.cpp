7f164f6e588ee88e4a0c158b25326d723de25f38
BlocksFunction <3398817447@qq.com>
2025-01-12 21:10:45 +0800

    压缩行数

--- src/core.cpp
+++ src/core.cpp
@@ -6,7 +6,6 @@
 #include <vector>
 
 namespace IO {
-// 字符串输入
 void scanfs(std::string &str) {
   char tmp;
   while ((tmp = static_cast<char>(getchar())) != '\n' && tmp != ' ' &&
@@ -14,55 +13,44 @@ void scanfs(std::string &str) {
     str += tmp;
 }
 
-// 单字符停止
 void scanfA(std::string &str, const char stopChar) {
   char tmp;
   while ((tmp = static_cast<char>(getchar())) != stopChar)
     str += tmp;
 }
 
-// 延迟输出
 void printfs(const std::string &str, double speed) {
-  if (!str.empty()) {
-    for (char c : str) {
-      putchar(c);
-      std::fflush(stdout);
-      std::this_thread::sleep_for(
-          std::chrono::milliseconds(static_cast<int>(speed)));
-    }
+  for (char c : str) {
+    putchar(c);
+    std::fflush(stdout);
+    std::this_thread::sleep_for(
+        std::chrono::milliseconds(static_cast<int>(speed)));
   }
 }
 } // namespace IO
 
 class File {
 public:
-  explicit File(const std::string &FileNames, bool *IsNew = nullptr) {
-    if (IsNew) {
-      *IsNew = fopen(FileNames.c_str(), "r") == nullptr;
-    }
-    this->FilePtr = fopen(FileNames.c_str(), "r+");
-    this->FileName = FileNames;
+  explicit File(const std::string &FileNames, bool *IsNew = nullptr)
+      : FileName(FileNames), FilePtr(fopen(FileNames.c_str(), "r+")) {
+    if (IsNew)
+      *IsNew = FilePtr == nullptr;
   }
 
   ~File() {
-    if (FilePtr) {
+    if (FilePtr)
       fclose(FilePtr);
-    }
   }
 
-  [[nodiscard]] bool Write(const std::string &str) const {
-    if (FilePtr && !str.empty()) {
-      fprintf(this->FilePtr, "%s", str.c_str());
-      return true;
-    }
-    return false;
+  bool Write(const std::string &str) const {
+    return FilePtr && !str.empty() && fprintf(FilePtr, "%s", str.c_str()) > 0;
   }
 
-  [[nodiscard]] std::vector<std::string> Read() const {
+  std::vector<std::string> Read() const {
     char line[1000];
     std::vector<std::string> arr;
-    rewind(this->FilePtr);
-    while (std::fgets(line, sizeof(line), this->FilePtr) != nullptr)
+    rewind(FilePtr);
+    while (std::fgets(line, sizeof(line), FilePtr))
       arr.push_back(line);
     return arr;
   }
@@ -76,22 +64,21 @@ int main() {
   printf("ZCode: 解释器\n文件地址: ");
   std::string filepath;
   IO::scanfA(filepath, '\n');
-  bool IsNew = true;
+  bool IsNew;
   File file(filepath, &IsNew);
-  if (IsNew)
+  if (IsNew) {
     printf("错误: %s 是一个不存在的文件\n", filepath.c_str());
-  else {
+  } else {
     for (std::string line : file.Read()) {
       if (line.find("Input") != std::string::npos) {
         line.erase(0, line.find("Input ") + 6);
         std::string text;
         int speed = 0;
         for (char c : line) {
-          if (isalpha(c) || c == ' ') {
+          if (isalpha(c) || c == ' ')
             text += c;
-          } else if (isdigit(c)) {
+          else if (isdigit(c))
             speed = speed * 10 + (c - '0');
-          }
         }
         if (speed)
           IO::printfs(text, speed);
